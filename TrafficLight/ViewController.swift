dd6bdd7b1171bc0c5103db2f63a29892a0ba64a6
Datsuk Maxim <datsuk.job@gmail.com>
2022-06-20 02:33:19 +0700

    Added switching logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,59 @@
 
 import UIKit
 
+private enum ColorType {
+    case red
+    case yellow
+    case green
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var actionButton: UIButton!
+    
+    private var colorType = ColorType.red
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = redView.bounds.width / 2
+        redView.backgroundColor = UIColor.red.withAlphaComponent(0.3)
+        
+        yellowView.layer.cornerRadius = yellowView.bounds.width / 2
+        yellowView.backgroundColor = UIColor.yellow.withAlphaComponent(0.3)
+        
+        greenView.layer.cornerRadius = greenView.bounds.width / 2
+        greenView.backgroundColor = UIColor.green.withAlphaComponent(0.3)
+        
+        actionButton.layer.cornerRadius = 10
     }
 
+    @IBAction func actionButtonTapped() {
+        actionButton.setTitle("NEXT", for: .normal)
 
+        switch colorType {
+        case .red:
+            changeViewsBackgroundColorWithAlpha(red: 1, yellow: 0.3, green: 0.3)
+            colorType = .yellow
+        case .yellow:
+            changeViewsBackgroundColorWithAlpha(red: 0.3, yellow: 1, green: 0.3)
+            colorType = .green
+        case .green:
+            changeViewsBackgroundColorWithAlpha(red: 0.3, yellow: 0.3, green: 1)
+            colorType = .red
+        }
+    }
+    
+    private func changeViewsBackgroundColorWithAlpha(
+        red: CGFloat,
+        yellow: CGFloat,
+        green: CGFloat
+    ) {
+        redView.backgroundColor = UIColor.red.withAlphaComponent(red)
+        yellowView.backgroundColor = UIColor.yellow.withAlphaComponent(yellow)
+        greenView.backgroundColor = UIColor.green.withAlphaComponent(green)
+    }
 }
 
